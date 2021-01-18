45d6f609676baf7be26706f7ce319e3d140a8f97
abbymakarim <abby.makarim@gmail.com>
2021-01-18 12:02:29 +0700

    initial commit

---
+++ __test__/user.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest')
+
+describe('POST /register',  function(){
+    it('should send response with 201 status code', function(){
+        //setup
+        //execute
+        //Assert
+    })
+})
+
+describe('POST /login',  function(){
+    it('should send response with 200 status code', function(){
+        //setup
+        //execute
+        //Assert
+    })
+})
