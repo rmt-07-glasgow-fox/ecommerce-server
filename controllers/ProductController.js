705d48ae37e24cdff9ce5d67b088ff60e15f02d8
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-18 17:25:00 +0700

    add: product methods

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,18 +1,88 @@
+const { Product } = require('../models')
+
 class ProductController {
   static getAllProducts(req, res, next){
-
+    Product.findAll()
+      .then(data => {
+        res.send(data)
+      })
+      .catch(err => {
+        res.send(err)
+      })
   }
   static getProductById(req, res, next){
-
+    Product.findByPk(req.params.id)
+      .then(data => {
+        if(data){
+          res.status(200).json(data)
+        }else{
+          next({
+            name: 'NoData'
+          })
+        }
+      })
+      .catch(err => {
+        next(err)
+      })
   }
   static addProduct(req, res, next){
-
+    Product.create({
+      name: req.body.name || '',
+      image_url: req.body.image_url || '',
+      price: +req.body.price,
+      stock: req.body.stock || '',
+      UserId: req.userData.id,
+    })
+      .then(data => {
+        res.status(201).json({
+          data
+        })
+      })
+      .catch(err => {
+        next(err)
+      })
   }
   static editProductById(req, res, next){
-
+    Product.update(req.body, {
+      where:{
+        id: req.params.id
+      }
+    })
+      .then(data => {
+        if(data[0] === 1){
+          res.status(200).json({
+            message: 'Success your product has been saved.'
+          })
+        }else{
+          next({
+            name: 'NoData'
+          })
+        }
+      })
+      .catch(err => {
+        next(err)
+      })
   }
   static deleteProductById(req, res, next){
-    
+    Product.destroy({
+      where:{
+        id: req.params.id
+      }
+    })
+      .then(data => {
+        if(data === 1){
+          res.status(200).json({
+            message: 'Success your product has been deleted.'
+          })
+        }else{
+          next({
+            name: 'NoData'
+          })
+        }
+      })
+      .catch(err => {
+        next(err)
+      })
   }
 }
 
