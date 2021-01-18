569329d0e6e0c8033d25dc023943d148aec413bd
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-18 17:23:37 +0700

    add: auith admin validation

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,70 @@
+const { User, Product } = require('../models')
+const { verifyToken } = require('../helpers/jwtHelper')
+
 const authentication = (req, res, next) => {
-  next()
+  try {
+    if(req.headers.access_token){
+      let decryptedData = verifyToken(req.headers.access_token)
+      if(decryptedData.role === 'admin'){
+        User.findOne({
+          where:{
+            email: decryptedData.email
+          }
+        })
+          .then(data => {
+            req.userData = data
+            next()
+          })
+          .catch(err => {
+            next({
+              name: 'WrongEmail'
+            })
+          })
+      }else{
+        next({
+          name: 'NotAdmin'
+        })
+      }
+    }else{
+      next({
+        name: 'Forbidden'
+      })
+    }
+  } catch (error) {
+    next({
+      name: 'InvalidToken'
+    })
+  }
 }
 
 const authorization = (req, res, next) => {
-  next()
+  const { id } = req.params
+  try {
+    Product.findOne({
+      where:{
+        id
+      }
+    })
+      .then(data => {
+        console.log(data, '<<< data')
+        console.log(req.userData, '<<< user data')
+        if(!data || data.UserId !== req.userData.id){
+          next({
+            name: 'Forbidden'
+          })
+        }else{
+          next()
+        }
+      })
+      .catch(err => {
+        console.log('here')
+        next(err)
+      })
+  } catch (error) {
+    next({
+      name: 'InvalidToken'
+    })
+  }
 }
 
 module.exports = {
