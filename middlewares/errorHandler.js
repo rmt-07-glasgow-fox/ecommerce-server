87722fa4ccb9e77ec7d9da01767cd41b1335a030
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-18 13:07:08 +0700

    add: error handler user login

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,41 @@
 const errorHandler = (err, req, res, next) => {
-  next()
+  if(err){
+    switch (err.name) {
+      case "WrongEmail":
+        res.status(401).json({
+          message: "Unauthorized. Something's went wrong check your email or password."
+        })
+        break;
+      case "WrongPassword":
+        res.status(401).json({
+          message: "Unauthorized. Something's went wrong check your email or password."
+        })
+        break;
+      default:
+        if(err.name === 'SequelizeValidationError'){
+          let errors = err.errors.map(el => {
+            return el.message
+          })
+          res.status(500).json({
+            errors
+          })
+          break;
+        }else if(err.name === 'SequelizeUniqueConstraintError'){
+          let errors = err.errors.map(el => {
+            return el.message
+          })
+          res.status(500).json({
+            errors
+          })
+          break;
+        }else{
+          res.status(500).json({
+            message: 'Internal server error.'
+          })
+          break;
+        }
+    }
+  }
 }
 
 module.exports = errorHandler
