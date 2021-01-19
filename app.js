f52803aa327e5ef0685de90148013cd0ef2e89df
imam hermawan <immd.flores@gmail.com>
2021-01-19 22:44:57 +0700

    add cors

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const errorHandler = require('./middlewares/errorHandler')
 const app = express()
 const router = require('./routes')
+const cors = require('cors')
 
-
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
