e22e4eeeb12ef89cc5b12c0502fee98d396903b0
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-19 09:14:52 +0700

    add: authorization middleware

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const ProductController = require('../controllers/ProductController')
+const authorization = require('../middlewares/auth').authorization
 
 router.post('/', ProductController.addProduct)
 router.get('/', ProductController.getAllProducts)
-router.get('/:id', ProductController.getProductById)
-router.put('/:id', ProductController.editProductById)
-router.delete('/:id', ProductController.deleteProductById)
+router.get('/:id', authorization, ProductController.getProductById)
+router.put('/:id', authorization, ProductController.editProductById)
+router.delete('/:id', authorization, ProductController.deleteProductById)
 
 module.exports = router
