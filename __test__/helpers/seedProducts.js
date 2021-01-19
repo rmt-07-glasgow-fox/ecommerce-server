a02a8ddd255e6ac5dc12c9f4f368f9fd0e95f305
ical10 <mochhusnir@gmail.com>
2021-01-19 09:01:04 +0700

    setting up tests for Users

--- __test__/helpers/seedProducts.js
+++ __test__/helpers/seedProducts.js
@@ -1,6 +1,6 @@
 const { Product } = require('../../models/')
 
-const users = [
+const products = [
     {
         name: 'nice headphones',
         image_url: 'https://media.wired.com/photos/5e7164aeb9399f00096a2ae6/1:1/w_1800,h_1800,c_limit/Gear-Mont-Blanc-Smart-Headphones-Gold-Front-SOURCE-Mont-Blanc.jpg',
@@ -12,7 +12,7 @@ const users = [
 ]
 
 const seedProducts = () => {
-    return Product.bulkCreate(users)
+    return Product.bulkCreate(products)
 }
 
 module.exports = seedProducts
--- __test__/user.test.js
+++ __test__/user.test.js
@@ -2,19 +2,25 @@ const request = require('supertest')
 const app = require('../app')
 const clearUsers = require('./helpers/clearUsers')
 const models = require('../models/')
+const seedUsers = require('./helpers/seedUsers')
 
 beforeAll((done) => {
     // create user di sini, jangan lupa hooks beforeCreate utk hash password
-})
-
-afterAll((done) => {
-    clearUsers()
+    seedUsers()
         .then(() => {
-            models.sequelize.close()
             done()
         })
         .catch(console.log)
 })
+
+//afterAll((done) => {
+//clearUsers()
+//.then(() => {
+//models.sequelize.close()
+//done()
+//})
+//.catch(console.log)
+//})
 
 describe('POST /login', () => {
 
