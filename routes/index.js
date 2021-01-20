ed4d9e2eb473682aadae01746738f4704dd9953c
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-20 20:32:17 +0700

    add: category endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router()
 const authRoutes = require('./authRoutes')
 const productRoutes = require('./productRoutes')
 const bannerRoutes = require('./bannerRoutes')
+const categoryRoutes = require('./categoryRoutes')
 const authentication = require('../middlewares/auth').authentication
 
 router.use(authRoutes)
 router.use(authentication)
 router.use('/products', productRoutes)
 router.use('/banners', bannerRoutes)
+router.use('/categories', categoryRoutes)
 
 module.exports = router
