aa8b3758797a3a1cad32e3eef130059dbc2762f2
Egha Dhiwangkara <Egha@Macbooks-MacBook-Pro.local>
2021-01-20 19:10:15 +0700

    server v4

--- routers/index.js
+++ routers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const login = require('./auth')
 const {authenticate} = require('../middlewares/auth')
 const product = require('./product')
+const banner = require('./banner')
 
 router.use(login)
 router.use(authenticate)
 router.use('/products', product)
+router.use('/banners', banner)
 
 module.exports = router
