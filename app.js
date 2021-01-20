c2a59fa59028a84303679924953e87f1058a741a
rozakus <abdulrozak.mail@gmail.com>
2021-01-21 00:26:47 +0700

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // express
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 // middlewares
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(express.static('assets'))
