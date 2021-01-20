c4663af8987e8d932805f5d4863fa48554f44fc6
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-20 20:32:00 +0700

    add: patch category by id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const ProductController = require('../controllers/ProductController')
-const authorization = require('../middlewares/auth').authorization
+const authorization = require('../middlewares/auth').productAuthorization
 
 router.post('/', ProductController.addProduct)
 router.get('/', ProductController.getAllProducts)
 router.get('/:id', authorization, ProductController.getProductById)
 router.put('/:id', authorization, ProductController.editProductById)
+router.patch('/:id', ProductController.patchCategoryById)
 router.delete('/:id', authorization, ProductController.deleteProductById)
 
 module.exports = router
