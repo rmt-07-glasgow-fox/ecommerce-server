007f8d9c609d7aae4379b4944e7360cfee1ee499
Ekomunthe48 <gudepsman.1silaen@gmail.com>
2021-01-20 10:54:36 +0700

    Done With The Server

---
+++ app.js
@@ -0,0 +1,20 @@
+if(process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test'){
+    require('dotenv').config()
+}
+
+const express = require('express');
+const app = express()
+const cors = require('cors')
+const routers = require('./routes')
+const { errHandlers } = require('./middlewares');
+
+app.use(express.urlencoded({ extended:false }))
+app.use(express.json())
+
+
+app.use(cors())
+
+app.use(routers)
+app.use(errHandlers)
+
+module.exports = app
