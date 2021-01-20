a8c98c27b58bae1351e342b72863605f6b1878dc
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-20 15:46:24 +0700

    add: banners

---
+++ routes/bannerRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const BannerController = require('../controllers/BannerController')
+const authorization = require('../middlewares/auth').authorization
+
+router.post('/', BannerController.addBanner)
+router.get('/', BannerController.getAllBanners)
+router.get('/:id', authorization, BannerController.getBannerById)
+router.put('/:id', authorization, BannerController.editBannerById)
+router.delete('/:id', authorization, BannerController.deleteBannerById)
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const authRoutes = require('./authRoutes')
 const productRoutes = require('./productRoutes')
+const bannerRoutes = require('./bannerRoutes')
 const authentication = require('../middlewares/auth').authentication
 
 router.use(authRoutes)
 router.use(authentication)
 router.use('/products', productRoutes)
+router.use('/banners', bannerRoutes)
 
 module.exports = router
