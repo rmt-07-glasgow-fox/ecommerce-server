8f1efd9947064eef375bf3a910c328b6c1fbaa25
raflirach <rafli060395@gmail.com>
2021-01-23 05:51:10 +0700

    add seeder for category

---
+++ seeders/20210122224921-seed-category.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Categories', [{
+    name: 'other',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkInsert('Categories', null, {});
+  }
+};
