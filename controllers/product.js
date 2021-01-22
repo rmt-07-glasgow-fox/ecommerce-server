9d6f5c1f8a9c0a0030c83c96ead435971f9abd27
ical10 <mochhusnir@gmail.com>
2021-01-22 16:44:56 +0700

    flow for Product CRUD - OK

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,7 @@
 const { Product } = require('../models/')
 
 class ProductController {
-  static create(req, res, next) {
+  static create (req, res, next) {
     const { name, image_url, price, stock } = req.body
     Product.create({
       name,
@@ -15,7 +15,7 @@ class ProductController {
       .catch(next)
   }
 
-  static update(req, res, next) {
+  static update (req, res, next) {
     const { stock, price, name, image_url } = req.body
     const id = +req.params.id
 
@@ -34,21 +34,29 @@ class ProductController {
       .catch(next)
   }
 
-  static delete(req, res, next) {
+  static delete (req, res, next) {
     const id = +req.params.id
 
-    Product.findByPk(id)
-      .then(() => {
-        const confirmDelete = [
-          'Product deleted'
-        ]
+    Product.destroy({
+      where: {
+        id
+      }
+    })
+      .then(result => {
+        if (result === 1) {
+          const confirmDelete = [
+            'Product deleted'
+          ]
 
-        res.status(200).json({ confirmDelete })
+          res.status(200).json({ confirmDelete })
+        } else {
+          next({ name: 'ResourceNotFound' })
+        }
       })
       .catch(next)
   }
 
-  static fetchAll(req, res, next) {
+  static fetchAll (req, res, next) {
     Product.findAll()
       .then(products => {
         res.status(200).json(products)
@@ -56,7 +64,7 @@ class ProductController {
       .catch(next)
   }
 
-  static fetchById(req, res, next) {
+  static fetchById (req, res, next) {
     const id = +req.params.id
     Product.findByPk(id)
       .then(product => {
