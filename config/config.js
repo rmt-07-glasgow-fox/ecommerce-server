7de10680505ca717683f6eb35ba60e5a02a97fb2
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-01-24 03:57:59 +0700

    update ssl heroku

--- config/config.js
+++ config/config.js
@@ -28,6 +28,6 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+    dialectOptions: { ssl: true },
   },
 };
