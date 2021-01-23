ccd9da64fc72213ddd63fd1756b038c140eace1b
DZAKYALR <0dzakyalr@gmail.com>
2021-01-24 01:19:27 +0700

    deploy3

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ const categories = require('./category')
 const banner = require('./banner')
 const { authenticate } = require('../middlewares/auth')
 
-
+router.get('/', (req, res) => {
+    res.send('Welcome to CMS-Used-Aircraft')
+  })
 router.use(authRouter)
 router.use(authenticate)
 router.use('/categories',categories )
