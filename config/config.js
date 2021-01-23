859db6f7ff6bc3a3f481a689269554bfd853599e
DZAKYALR <0dzakyalr@gmail.com>
2021-01-24 01:08:25 +0700

    'deploy2'

--- config/config.js
+++ config/config.js
@@ -26,6 +26,11 @@ module.exports = {
     dialect 
   },
   "production": {
-    "use_env_variable": "DATABASE_URL"
+    "use_env_variable": "DATABASE_URL",
+    "dialectOptions":{
+      "ssl": {
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
