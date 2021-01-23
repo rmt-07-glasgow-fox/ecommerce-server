18241b3ca5ca7c1399c10bd7a370d27a7721a5f2
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-24 05:06:28 +0700

    config

--- config/config.js
+++ config/config.js
@@ -25,10 +25,10 @@ module.exports = {
 		dialect
 	},
   "production": {
-		"username": "root",
-		"password": null,
+		"username": "yppdhujxisxkjn",
+		"password": "371ee09837336f084f1668ecffd7efd3d08aa7fc3d6f4617cded14792980fa62",
 		"database": "database_production",
-		"host": "127.0.0.1",
-		"dialect": "mysql"
+		"host": "ec2-54-85-13-135.compute-1.amazonaws.com",
+		"dialect": "postgres"
 	}
 }
