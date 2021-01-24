eee96d355348a2d357e305fdba47bd531e1121d9
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-24 14:45:17 +0700

    [nanda] update config production

--- config/config.js
+++ config/config.js
@@ -11,25 +11,27 @@ const dialect = process.env['DB_DIALECT_' + upperCaseEnv];
 
 
 module.exports = {
-    development: {
-        username,
-        password,
-        database,
-        host,
-        dialect,
-    },
-    test: {
-        username,
-        password,
-        database,
-        host,
-        dialect,
-    },
-    production: {
-        username: "root",
-        password: null,
-        database: "database_production",
-        host: "127.0.0.1",
-        dialect: "mysql"
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect,
+  },
+  test: {
+    username,
+    password,
+    database,
+    host,
+    dialect,
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
+  }
 }
