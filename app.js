bee4f2f23642b0f0737b4beef719444519cec63c
Ekomunthe48 <gudepsman.1silaen@gmail.com>
2021-01-24 13:06:02 +0700

    fixing the cors

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ const cors = require('cors')
 const routers = require('./routes')
 const { errHandlers } = require('./middlewares');
 
-app.use(express.urlencoded({ extended:false }))
+app.use(express.urlencoded({ extended:true }))
 app.use(express.json())
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    if (req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json({})
-    }
-    next()
-})
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+//     if (req.method === 'OPTIONS'){
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+//         return res.status(200).json({})
+//     }
+//     next()
+// })
 app.use(cors())
 
 app.use(routers)
