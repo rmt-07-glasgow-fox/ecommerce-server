3db6ed3d7fc3e8240ee13ebbd04ab728bce85773
dayuhidayatullah <dayuhidayat123@gmail.com>
2021-01-24 14:35:47 +0700

    change-migration-typo

--- migrations/20201216102702-Add-column-status-table-cart.js
+++ migrations/20201216102702-Add-column-status-table-cart.js
@@ -18,6 +18,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-    await queryInface.removeColumn('Carts', 'status', {})
+    await queryInterface.removeColumn('Carts', 'status', {})
   }
 };
