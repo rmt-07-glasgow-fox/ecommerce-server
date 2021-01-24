801c6ddc9e315b1c5a9ea6df3feeada1ceb0193f
Edwin Dharmawan <edwindharmawan@gmail.com>
2021-01-24 17:52:36 +0700

    add: get by id

--- controller/product.js
+++ controller/product.js
@@ -10,6 +10,16 @@ class productController {
             next(err)
         }
     }
+    static async getId (req, res, next) {
+        try {
+            const read = await Product.findOne({
+                where: { id:req.params.id}
+            })
+            res.status(200).json(read)
+        } catch (err) {
+            next(err)
+        }
+    } 
     static async createProduct (req, res, next) {
         const { name, image_url, price, stock } = req.body
 
--- routers/product.js
+++ routers/product.js
@@ -3,6 +3,7 @@ const productControl = require('../controller/product')
 const { authorization } = require('../middleware/auth')
 
 product.get('/products', productControl.readProduct)
+product.get('/products/:id', productControl.getId)
 product.post('/products', authorization,productControl.createProduct)
 product.put('/products/:id', authorization, productControl.update)
 product.delete('/products/:id', authorization, productControl.delete)
