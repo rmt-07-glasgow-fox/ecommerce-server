f9df8e960516aab08c81c4b71e5767f240460b2d
samm021 <mohammed.ismail.pribadi@gmail.com>
2021-01-24 16:50:58 +0700

    heroku please run

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 if (process.env.NODE_DEV === 'development' || process.env.NODE_ENV === 'test') {
     require('dotenv').config();
 }
-
 const express = require('express');
 const app = express();
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const cors = require('cors');
+const port = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -15,5 +16,4 @@ app.use(express.urlencoded({
 }));
 app.use(router);
 app.use(errorHandler);
-
-module.exports = app;
+app.listen(port, _=> console.log(`running on port: ${port}`));
