34b35b18e67ef59f78fcf7b1cf4cdaf003f1446c
kanzff <rivakanzf@gmail.com>
2021-01-24 12:59:34 +0700

    seed Admin

---
+++ seeders/20210124054408-seedAdmin.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+const { now } = require('sequelize/types/lib/utils');
+const salt = bcrypt.genSaltSync(5)
+const adminPass = bcrypt.hashSync('1234', salt)
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+      await queryInterface.bulkInsert('Users', [{
+        email:'admin@mail.com',
+        password: adminPass,
+        role: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+};
