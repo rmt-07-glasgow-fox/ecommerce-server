28b952a81e5181c0abc4cd9ec06b9799cccdf1ef
ekydpa21 <ekydodipratamaa21@gmail.com>
2021-01-24 14:29:01 +0700

    all clear, deploy

--- seeders/20210118134635-seed-user.js
+++ seeders/20210118134635-seed-user.js
@@ -1,19 +1,18 @@
 'use strict';
-const fs = require("fs/promises")
 const {hasher} = require("../helpers/hash")
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return fs.readFile("./seeders/user.json", "utf-8")
-    .then(data => {
-      data = JSON.parse(data)
-      data.forEach(el => {
-        el.password = hasher(el.password)
-        el.createdAt = new Date()
-        el.updatedAt = new Date()
-      })
-      return queryInterface.bulkInsert("Users", data, {})
-    })
+    return queryInterface.bulkInsert("Users", [
+      {
+        name: "Admin 1",
+        email: "admin@mail.com",
+        password: hasher("123123"),
+        role: "Admin",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {})
   },
 
   down: (queryInterface, Sequelize) => {
