773875af561bcdc64d0882bcd01321c98f622de4
Ekomunthe48 <gudepsman.1silaen@gmail.com>
2021-01-24 12:31:08 +0700

    deploy to heroku

--- config/config.js
+++ config/config.js
@@ -25,6 +25,12 @@ module.exports = {
     dialect
   },
   "production": {
-    "use_env_variable": "DATABASE_URL"
+    "use_env_variable": "DATABASE_URL",
+    "dialectOptions": {
+      "ssl": {
+          require : true,
+          rejectUnauthorized: false
+       }
+    }
   }
 }
