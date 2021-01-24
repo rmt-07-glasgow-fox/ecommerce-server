40ff1570fbe2f90c4e55a9f29ff3b7bdf63f2f94
riokp <riokp89@gmail.com>
2021-01-24 13:39:13 +0700

    config added

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ const express = require("express");
 const app = express();
 const router = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
-const port = process.env.PORT || 3000
 const cors = require('cors');
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
+app.use(router)
 
 app.use(errorHandler)
-app.use(router)
 
 module.exports = app
