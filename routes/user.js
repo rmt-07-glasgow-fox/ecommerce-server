b384c89327de97a83de11f9eb68905c64f30f29c
dayuhidayatullah <dayuhidayat123@gmail.com>
2021-01-24 14:05:32 +0700

    add-router-get-/

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const UserController = require('../controllers/user')
 
+router.get('/', (req, res) => {
+    res.send('<h1>HELOO BOSS</h1>')
+})
 router.post('/register', UserController.register)
 router.post('/login/admin', UserController.loginAdmin)
 router.post('/login/customer', UserController.loginCustomer)
