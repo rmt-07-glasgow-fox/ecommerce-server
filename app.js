9d9ead8e211a7d2203cc3c3a5570b41b06a9cd83
samm021 <mohammed.ismail.pribadi@gmail.com>
2021-01-24 17:48:37 +0700

    ecom server final commit - all done, changed start script

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 if (process.env.NODE_DEV === 'development' || process.env.NODE_ENV === 'test') {
     require('dotenv').config();
 }
+
 const express = require('express');
 const app = express();
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const cors = require('cors');
-const port = process.env.PORT || 3000;
-
 
 app.use(cors());
 app.use(express.json());
@@ -16,4 +15,5 @@ app.use(express.urlencoded({
 }));
 app.use(router);
 app.use(errorHandler);
-app.listen(port, _=> console.log(`running on port: ${port}`));
+
+module.exports = app;
