73dfa2c43da3e9aeb911baf6d293a27946418b59
ahmad-akbar <ahmd.akbr25@gmail.com>
2021-01-24 08:14:42 +0800

    add get all product

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// if (process.env.NODE_ENV === "development") {
-//     require('dotenv').config()
-// }
+if (process.env.NODE_ENV === "development") {
+    require('dotenv').config()
+}
 
-const dotenv = require('dotenv').config()
+// const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
@@ -13,8 +13,8 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(router)
 
-// app.listen(port, () => {
-//     console.log(`Running on port ${port}`);
-// })
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+})
 
 module.exports = app
--- routes/routerProduct.js
+++ routes/routerProduct.js
@@ -1,7 +1,7 @@
 const ProductController = require('../controller/productController')
 const router = require('express').Router()
  
-
+router.get('/', ProductController.getall)
 router.post('/create', ProductController.create)
 router.put('/update/:id', ProductController.update)
 router.delete('/delete/:id', ProductController.delete)
