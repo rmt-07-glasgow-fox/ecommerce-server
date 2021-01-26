de8273e0d9b81fc5003da11e06107a5df0f13f53
ical10 <mochhusnir@gmail.com>
2021-01-27 06:31:03 +0700

    add updateCart,removeCart,fixing null error in getOneCart

--- controllers/cart.js
+++ controllers/cart.js
@@ -26,21 +26,22 @@ class CartController {
       .then((foundCart) => {
         if (!foundCart) {
           next({ name: 'ResourceNotFound' })
+        } else {
+          res.status(200).json(foundCart)
         }
-        res.status(200).json(foundCart)
       })
       .catch(next)
   }
 
   // for when we click on a button Add to cart
   static async addToCart (req, res, next) {
-    const UserId = req.user.id
-    const { ProductId, quantity } = req.body
-
-    let currentCart
-    let productStock = 0
-
     try {
+      const UserId = req.user.id
+      const quantity = +req.body.quantity
+      const ProductId = +req.body.ProductId
+
+      let currentCart
+      let productStock = 0
 
       const product = await Product.findOne({
         where: {
@@ -87,13 +88,66 @@ class CartController {
   }
 
   // update a cart
-  static updateCart (req, res, next) {
+  static async updateCart (req, res, next) {
+    try {
+      const cartId = +req.params.cartId
+      const quantity = +req.body.quantity
+      const ProductId = +req.body.ProductId
 
+      let currentCart
+      let productStock = 0
+
+      const product = await Product.findOne({
+        where: {
+          id: ProductId
+        }
+      })
+
+      if (product.stock >= quantity) {
+        productStock = product.stock
+        currentCart = await Cart.findByPk(cartId)
+      } else {
+        next({ name: 'StockExceeded' })
+      }
+
+      if (!currentCart) {
+        next({ name: 'ResourceNotFound' })
+      } else {
+        const newQuantity = currentCart.quantity + quantity
+        if (newQuantity <= productStock) {
+          let updatedCart = await currentCart.update({
+            quantity: newQuantity
+          })
+          const message = ['Cart updated']
+          res.status(200).json({ message })
+        } else {
+          next({ name: 'StockExceeded' })
+        }
+      }
+    } catch (err) {
+      next(err)
+    }
   }
 
   // for when we want to cancel a cart
   static removeCart (req, res, next) {
+    const cartId = +req.params.cartId
 
+    Cart.destroy({
+      where: {
+        id: cartId
+      }
+    })
+      .then(result => {
+        if (result === 1) {
+          const message = ['Product removed from cart']
+
+          res.status(200).json({ message })
+        } else {
+          next({ name: 'ResourceNotFound' })
+        }
+      })
+      .catch(next)
   }
 }
 
