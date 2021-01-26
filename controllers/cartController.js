fcae9288a44a21a0c250e787dcda2e6b2e46b811
rozakus <abdulrozak.mail@gmail.com>
2021-01-27 00:20:27 +0700

    router cart done + validations, but needed to check

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -39,6 +39,7 @@ class CartController {
         let updatedQuantity = quantity + dbCart.quantity
         let updatedTotalPrice = totalPrice + dbCart.totalPrice
 
+        // check stock
         if (updatedQuantity > dbProduct.stock) {
           return next({ name: 400, message: `please order below current stock ${dbProduct.stock}` })
         }
@@ -58,6 +59,85 @@ class CartController {
       }
 
       console.log('>>> check addCart flow control salah')
+
+    } catch (err) {
+      return next(err)
+    }
+  }
+
+  static async getAllCart(req, res, next) {
+    try {
+
+      let allCart = await Cart.findAll({
+        where: { UserId: req.user.id },
+        order: [['id']],
+        attributes: { exclude: ['createdAt', 'updatedAt'] },
+        include: [{
+          model: Product,
+          attributes: { exclude: ['createdAt', 'updatedAt'] }
+        },
+        {
+          model: User,
+          attributes: { exclude: ['createdAt', 'updatedAt', 'password'] }
+        }]
+      })
+
+      return res.status(200).json(allCart)
+
+    } catch (err) {
+      return next(err)
+    }
+  }
+
+  static async deleteCart(req, res, next) {
+    try {
+      let idCart = +req.params.id
+
+      // check cart is avail
+      let cart = await Cart.findByPk(idCart)
+
+      if (!cart) {
+        return next({ name: 404, message: 'Cart is not found' })
+      }
+
+      if (cart.UserId !== req.user.id) {
+        return next({ name: 401 })
+      }
+
+      await Cart.destroy({ where: { id: idCart } })
+
+      return res.status(200).json({ message: 'Cart is deleted' })
+
+    } catch (err) {
+      return next(err)
+    }
+  }
+
+  static async updateQuantity(req, res, next) {
+    try {
+      let idCart = +req.params.id
+      let updatedQuantity = +req.body.quantity
+
+      // validate quantity
+      if (!updatedQuantity) { return next({ name: 400, message: 'quantity is required and should higher than 0' }) }
+
+      // check cart is avail
+      let cart = await Cart.findByPk(idCart)
+
+      if (!cart) { return next({ name: 404, message: 'Cart is not found' }) }
+      if (cart.UserId !== req.user.id) { return next({ name: 401 }) }
+
+      // check productId
+      let dbProduct = await Product.findByPk(cart.ProductId)
+      console.log('>>> dbProduct', dbProduct)
+
+      if (!dbProduct) { return next({ name: 404, message: 'product not found' }) }
+      if (dbProduct.stock < updatedQuantity) { return next({ name: 400, message: `please order below current stock ${dbProduct.stock}` }) }
+
+      console.log('>>> berhasil dong !!')
+
+      let updatedCart = await Cart.update({ quantity: updatedQuantity }, { where: { id: idCart } })
+      return res.status(200).json({ message: `Cart ${dbProduct.name} quantity is updated to ${updatedQuantity}` })
 
     } catch (err) {
       return next(err)
--- router/cartRouter.js
+++ router/cartRouter.js
@@ -2,5 +2,8 @@ const router = require('express').Router()
 const CartController = require('../controllers/cartController')
 
 router.post('/', CartController.addCart)
+router.get('/', CartController.getAllCart)
+router.delete('/:id', CartController.deleteCart)
+router.patch('/:id', CartController.updateQuantity)
 
 module.exports = router
