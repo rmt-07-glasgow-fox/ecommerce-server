fdeffde5c35f7824252bdae1c02ef1428b5aa478
ical10 <mochhusnir@gmail.com>
2021-01-27 05:28:48 +0700

    cleaning up code for cartsRouter

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const UserController = require('../controllers/user.js')
 const { authenticate } = require('../middlewares/auth')
 
 const productsRouter = require('./products')
+const cartsRouter = require('./carts')
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 
 router.use('/products', authenticate, productsRouter)
+router.use('/carts', authenticate, cartsRouter)
 
 module.exports = router
 
--- routes/products.js
+++ routes/products.js
@@ -3,8 +3,11 @@ const router = express.Router()
 const ProductController = require('../controllers/product')
 const { authorize } = require('../middlewares/auth')
 
+// Available for users and admin
 router.get('/', ProductController.fetchAll)
 router.get('/:id', ProductController.fetchById)
+
+// only accessible for admin
 router.post('/', authorize, ProductController.create)
 router.put('/:id', authorize, ProductController.update)
 router.delete('/:id', authorize, ProductController.delete)
