412774aa83588352a37b6197c2d1d2b497355d63
ical10 <mochhusnir@gmail.com>
2021-01-27 05:25:09 +0700

    fixing migration files to get proper associations in Carts

---
+++ migrations/20210126031506-create-cart-item.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return Promise.resolve()
+  },
+  down: async (queryInterface, Sequelize) => {
+    return Promise.resolve()
+  }
+};
