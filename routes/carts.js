bc384970721a5ddf978083101356e556b547ac8c
ical10 <mochhusnir@gmail.com>
2021-01-27 05:38:28 +0700

    adding required routes for Carts

---
+++ routes/carts.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const CartController = require('../controllers/cart')
+
+router.get('/', CartController.fetchAllCarts)
+router.get('/:cartId', CartController.fetchOneCart)
+router.post('/', CartController.addToCart)
+router.patch('/:cartId', CartController.updateCart)
+router.delete('/:cartId', CartController.removeCart)
+
+module.exports = router
