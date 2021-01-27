3cf99077ba9c86d864feeb10ed1bea44de875505
TommySus <tommysusanto77@gmail.com>
2021-01-27 14:00:58 +0700

    done

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -2,28 +2,33 @@ const { History, Product, Cart } = require('../models')
 
 class HistoryController {
     static addHistory (req,res,next) {
-        const id = req.body.allCartId
-        Cart.destroy({where: {id: id}})
+        let id = []
+        let obj = []
+        for (let i = 0; i < req.body.length; i++) {
+            obj.push({
+                UserId: req.loggedInUser.id,
+                ProductId: req.body[i].ProductId,
+                total: req.body[i].total
+            })
+            id.push(req.body[i].allCartId)
+        }
+        History.bulkCreate(obj)
         .then(data => {
-            if (data) {
-                const obj = {
-                    UserId: req.loggedInUser.id,
-                    ProductId: req.body.ProductId,
-                    total: req.body.total
+            res.status(201).json(data)
+            return Cart.destroy({where: {id: id}})
+            .then(data => {
+                if (data) {
+                    res.status(200).json({message: 'cart deleted'})
+                } else {
+                    throw {
+                        status: 404, 
+                        message: 'cart not found'
+                    }
                 }
-                return History.create(obj)
-                .then(data => {
-                    res.status(201).json(data)
-                })
-                .catch(error => {
-                    next(error)
-                })
-            } else {
-                throw {
-                    status: 404, 
-                    message: 'cart not found'
-                }
-            }
+            })
+            .catch(error => {
+                next(error)
+            })
         })
         .catch(error => {
             next(error)
