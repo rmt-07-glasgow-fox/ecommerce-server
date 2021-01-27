d873537a7124302008e56e3dea4d2cd38e2cc4ee
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-27 09:56:45 +0700

    [nanda] fix path method delete

--- routes/cart.js
+++ routes/cart.js
@@ -3,6 +3,6 @@ const cartController = require('../controllers/cartController')
 
 router.get('/cart', cartController.getAll);
 router.post('/cart/add', cartController.createOrUpdateCart);
-router.delete('/cart/:id/product/:productId', cartController.createOrUpdateCart);
+router.delete('/cart/:id/product/:productId', cartController.destroy);
 
 module.exports = router
