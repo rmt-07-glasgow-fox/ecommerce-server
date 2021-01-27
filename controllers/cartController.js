6cd166b9a247153e2d89173a359bc234f5990c49
Hani Alfiyyah <hanii.alfiyyah@gmail.com>
2021-01-27 22:00:45 +0700

    fixed create card and update quantity

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -37,43 +37,85 @@ class CartController {
   }
 
   static create(req, res, next) {
-    const { ProductId, quantity } = req.body
+    const ProductId = +req.body.ProductId
+    const quantity = +req.body.quantity
     const UserId = req.UserData.id
-    Cart
-      .findOne({
-        where: { ProductId }
+    let product = null
+
+    Product
+      .findOne({ // find to check stock
+        where: { id: ProductId },
+        attributes: {exclude: ['createdAt', 'updatedAt']}
       })
       .then(data => {
-        return !data ? Cart.create({ ProductId, quantity, UserId }) :
-        Cart.update({ quantity }, {
-          where: { ProductId }
-        })
+        product = data
+        return !data ?
+          next({ name: 'CustomError', statusCode: 404, message: 'Product Not Found' }) :
+        data && quantity > data.stock ?
+          next({ name: 'CustomError', statusCode: 400, message: 'Out of stock!' }) :
+          Cart.findOne({ where: { ProductId, UserId } }) // find to decide create or update
       })
       .then(data => {
-        res.status(200).json(data)
+        return !data ?
+          Cart.create({ ProductId, quantity, UserId }) : // create
+        data && data.quantity + quantity > product.stock ?
+          next({ name: 'CustomError', statusCode: 400, message: 'Out of stock!' }) :
+          Cart.update({ quantity: data.quantity + quantity }, {
+            where: { id: data.id },
+            returning: true
+          })
+      })
+      .then(data => {
+        if (data && data[1]) {
+          const { id, UserId, ProductId, quantity } = data[1][0].dataValues
+          res.status(200).json({ id, UserId, ProductId, quantity }) // update
+        } else if (data && !data[1]) {
+          const { id, UserId, ProductId, quantity } = data
+          res.status(201).json({ id, UserId, ProductId, quantity }) // create
+        } else {
+          next({ name: 'CustomError', statusCode: 404, message: 'Cart Not Found' })
+        }
       })
       .catch(next)
   }
 
   static updateQty(req, res, next) {
-    const { quantity } = req.body
-    Cart
-      .update({ quantity }, {
-        where: { id: +req.params.id },
-        returning: true
-      })
-      .then(data => {
-        if (!data[1][0]) next({ name: 'CustomError', statusCode: 404, message: 'Cart Not Found' })
-        else {
-          const { id, quantity } = data[1][0].dataValues
-          res.status(200).json({ id, quantity })
-        }
-      })
-
+    const quantity = +req.body.quantity
+    const id = +req.params.id
+    let cart = null
+    Cart.findOne({
+      where: { id }
+    })
+    .then(data => {
+      cart = data
+      return !data ?
+        next({ name: 'CustomError', statusCode: 404, message: 'Cart Not Found' }) :
+        Product.findOne({ where: { id: data.ProductId } })
+    })
+    .then(product => {
+      return !product ?
+        next({ name: 'CustomError', statusCode: 404, message: 'Product Not Found' }) :
+      cart.quantity + quantity > product.stock ?
+        next({ name: 'CustomError', statusCode: 400, message: 'Out of stock!' }) :
+        Cart.update({ quantity: cart.quantity + quantity },{
+          where: { id },
+          returning: true
+        })
+    })
+    .then(data => {
+      !data ?
+        next({ name: 'CustomError', statusCode: 404, message: 'Cart Not Found' }) :
+        res.status(200).json({
+          id: data[1][0].dataValues.id,
+          UserId: data[1][0].dataValues.UserId,
+          ProductId: data[1][0].dataValues.ProductId,
+          quantity: data[1][0].dataValues.quantity
+        }) // update
+    })
+    .catch(next)
   }
 
   static delete(req, res, next) {
-    console.log(req.params.id);
     Cart
       .destroy({
         where: { id: +req.params.id },
