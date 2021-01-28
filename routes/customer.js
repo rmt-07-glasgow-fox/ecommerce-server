e3b7d76bd3504e38ac6bf8b0039194459f4adf5d
riokp <riokp89@gmail.com>
2021-01-28 12:39:54 +0700

    routes fix

--- routes/customer.js
+++ routes/customer.js
@@ -1,13 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const ProductController = require("../controllers/productController")
-const authorization = require('../middlewares/authorization')
 const {authenticate} = require('../middlewares/authenticate')
 
 router.get(`/products`, ProductController.getAll)
 router.use(authenticate)
 router.get(`/products/:id`, ProductController.getById)
-router.use(authorization);
 router.post(`/products`, ProductController.add)
 router.put(`/products/:id`, ProductController.update)
 router.delete(`/products/:id`, ProductController.delete)
