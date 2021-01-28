2ed29fdf22cb4937ac380980feb9a16246f347a4
Farhad Zuhdi <farhad_zuhdi@yahoo.com>
2021-01-28 10:56:31 +0700

    Farhad Zaman Zuhdi

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router()
 const CartController = require('../controllers/cartController')
 const ProductController = require('../controllers/productController')
-const { authentication } = require('../middlewares/auth')
+const { authentication, authorizationCustomer } = require('../middlewares/auth')
 
 
 router.get('/products', ProductController.getProductHandler)
 router.use(authentication)
-router.get('/carts', CartController.getAllCartHandler)
-router.post('/carts', CartController.postAllCartHandler)
-router.patch('/carts', CartController.patchCartHandler)
-router.delete('/carts', CartController.deleteCartHandler)
+router.get('/carts', authorizationCustomer, CartController.getAllCartHandler)
+router.post('/carts', authorizationCustomer, CartController.postAllCartHandler)
+router.patch('/carts', authorizationCustomer, CartController.patchCartHandler)
+router.delete('/carts', authorizationCustomer, CartController.deleteCartHandler)
 
 module.exports = router
