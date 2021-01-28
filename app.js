91f2ca747522abb7b08db81251e9c98da7f2d9c4
kanzff <rivakanzf@gmail.com>
2021-01-28 11:40:45 +0700

    deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const routes = require('./routes')
 const { errorHandler } = require('./middlewares/error_handler')
-// const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 const app = express()
 
@@ -14,8 +14,8 @@ app.use('/', routes)
 app.use(errorHandler)
 
 
-// app.listen(port, () => {
-//     console.log(`app running on ${port}`)
-// })
+app.listen(port, () => {
+    console.log(`app running on ${port}`)
+})
 
 module.exports = app
