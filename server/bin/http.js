e41677e7af5fdafec1374e49e917f661e05f49c9
gianRVN <gianmohar@gmail.com>
2021-01-28 13:05:33 +0700

    final

--- server/bin/http.js
+++ server/bin/http.js
@@ -1,5 +1,5 @@
 const app = require('../app')
-const port = process.env.PORT || 3411
+const port = process.env.PORT || 3415
 
 app.listen(port, () => {
     console.log(`Connected on Port ${port}`)
--- server/models/user.js
+++ server/models/user.js
@@ -2,24 +2,53 @@
 const {
   Model
 } = require('sequelize');
+const { hashPassword } = require('../helpers/bcrypt');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Banner, {foreignKey: 'UserId', targetKey: 'id'})
+      this.hasMany(models.Wishlist, {foreignKey: 'UserId', targetKey: 'id'})
+      this.belongsToMany(models.Content, {through: models.Wishlist, foreignKey: 'UserId'})
+      this.hasMany(models.Cart, {foreignKey: 'UserId', targetKey: 'id'})
+      this.belongsToMany(models.Content, {through: models.Cart, foreignKey: 'UserId'})
     }
   };
   User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Email is required"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Password is required"
+        }
+      }
+    },
+    role: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Role is required"
+        }
+      }
+    },
   }, {
     sequelize,
     modelName: 'User',
+    hooks: {
+      beforeCreate: (instance, option) => {
+        instance.password = hashPassword(instance.password)
+      }
+    }
   });
   return User;
 };
