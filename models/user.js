85e12b6bf58202a16e6087b41f25d15d6b3d7dd9
DZAKYALR <0dzakyalr@gmail.com>
2021-01-28 07:06:07 +0700

    model

--- models/user.js
+++ models/user.js
@@ -56,7 +56,6 @@ module.exports = (sequelize, DataTypes) => {
     hooks: {
       beforeCreate: (user, options) => {
         user.password = hashPassword(user.password)
-        user.role = "costumer"
       }
     }
   });
