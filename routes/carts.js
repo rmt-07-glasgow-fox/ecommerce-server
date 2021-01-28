913a511fbee0997ae13a4c2ceceeeac7f52f54eb
ical10 <mochhusnir@gmail.com>
2021-01-28 11:09:17 +0700

    adding endpoint for checkout

--- routes/carts.js
+++ routes/carts.js
@@ -6,5 +6,6 @@ router.get('/:cartId', CartController.fetchOneCart)
 router.post('/', CartController.addToCart)
 router.patch('/:cartId', CartController.updateCart)
 router.delete('/:cartId', CartController.removeCart)
+router.patch('/', CartController.checkOut)
 
 module.exports = router
