33e5b83a7075374474e399267ee74bb70646d408
phitonthel <thephitonthel@gmail.com>
2021-03-08 16:33:05 +0700

    add dialect etc

--- config/config.js
+++ config/config.js
@@ -28,8 +28,12 @@ module.exports = {
   },
   "production": {
     "use_env_variable": "DATABASE_URL",
-    "ssl": {
-      "rejectUnauthorized": false
+    dialect: 'postgres',
+    "dialectOptions": {
+      "ssl": {
+        "require" : true,
+        "rejectUnauthorized": false
+      }
     }
   }
 }
